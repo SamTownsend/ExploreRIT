6d1137759c5e1472a64b3d1bb738ff8cf5916a0f
Adriano V <asv4740@rit.edu>
2016-12-05 21:45:24 -0500

    Documentation

--- app/src/main/java/com/example/explorerit/MapsActivity.java
+++ app/src/main/java/com/example/explorerit/MapsActivity.java
@@ -5,7 +5,6 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -16,38 +15,51 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    /**
+     * The GoogleMap object
+     */
     private GoogleMap mMap;
 
-    private LatLng home;
+    /**
+     * The target location for the user to get to
+     */
+    private LatLng target;
 
-    /*Values for random coordinates on the RIT campus to be chosen between*/
+    /**
+     * Values for random coordinates on the RIT campus to be chosen between
+     **/
     private static final double MIN_LAT = 43.082979;
     private static final double MAX_LAT = 43.086787;
     private static final double MIN_LONG = -77.681430;
     private static final double MAX_LONG = -77.667284;
-
+    /**
+     * The LatLng value designated as the "center" of the map
+     */
     private static final LatLng CENTER = new LatLng(((MAX_LAT + MIN_LAT) / 2) - .001000
             , ((MAX_LONG + MIN_LONG) / 2) - .001000);
-
+    /**
+     * The boundary in which the camera will stay in
+     */
     private static final LatLngBounds BOUNDARY = new LatLngBounds(new LatLng(MIN_LAT, MIN_LONG),
             new LatLng(MAX_LAT, MAX_LONG));
 
+    /**
+     * Didn't really mess with this much other then set to landscape
+     * @param savedInstanceState It's required
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-
     /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera
+     * Seems to work as an init method
+     * As a result, does all initialization
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -56,10 +68,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setMinZoomPreference(15);
         mMap.setMaxZoomPreference(19);
         mMap.setLatLngBoundsForCameraTarget(BOUNDARY);
-        //home = new LatLng(43.084644, -77.679988);
-        home = CENTER;
-        mMap.addMarker(new MarkerOptions().position(home).title("Next Destination"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(home));
+
+        target = CENTER;
+        mMap.addMarker(new MarkerOptions().position(target).title("Next Destination"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(target));
     }
 
     /**
@@ -71,9 +83,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Math.random() * (MAX_LONG - MIN_LONG) + MIN_LONG);
     }
 
+    /**
+     * An event handler for the button
+     * @param view It's required
+     */
     public void onSucc(View view){
         //home = getRandomLocation();
-        mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(home , 15.0f) );
+        mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(target , 15.0f) );
         System.out.println("You will never get the succ");
     }
 }
